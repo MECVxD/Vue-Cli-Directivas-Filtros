ed0d5123fd94a15f993fa826821cc3a1cead9fa2
Mauricio Covarrubias <mauricioecovarrubias@hotmail.com>
2021-01-29 22:03:03 -0600

    Add directives

--- src/main.js
+++ src/main.js
@@ -1,6 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+Vue.directive('decorar', {
+  bind(el, binding){
+    el.style.fontFamily=binding.value.familia
+    el.style.color=binding.value.color
+    if(binding.arg == 'grande'){
+      el.style.fontSize='50px'
+    }
+    if(binding.arg == 'pequeno'){
+      el.style.fontSize='10px'
+    }
+    if(binding.modifiers['negrilla']){
+      el.style.fontWeight='bold'
+    }
+    if(binding.modifiers['italico']){
+      el.style.fontStyle='italic'
+    }  
+  }
+})
+
+Vue.filter('suspensivos', function(texto){
+  return texto.substring(0, 10)+'...'
+})
+
 Vue.config.productionTip = false
 
 new Vue({
